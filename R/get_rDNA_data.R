ec675844654adc14772cf66172ecacae28cf1774
danvau98 <daniel.1.vaughan@kcl.ac.uk>
2026-08-21 16:38:56 +0100

    updates to the function.

--- R/get_rDNA_data.R
+++ R/get_rDNA_data.R
@@ -2,23 +2,24 @@
 #'
 #' Extracts all data associated with the rDNA chromosome from a `meth_object`.
 #'
+#' @name get_rDNA_data
 #' @param meth_object A methylation object (e.g., from `methylKit`).
 #' @param num_samples Number of samples in `meth_object` to process.
 #' @param rDNA_chr_name Name of the rDNA chromosome (default: `"rDNAm"`).
 #'
 #' @return A subset of `meth_object` containing only the rDNA chromosome data.
 #' @examples
-#' get_rDNA_data(meth_object, num_samples = 10, rDNA_chr_name)
-library("GenomicRanges")
+#' get_rDNA_data(meth_object, num_samples = 10, rDNA_chr_name = "rDNAm")
+#' @export
 get_rDNA_data <- function(
     meth_object,
     num_samples,
-    rDNA_chr_name
+    rDNA_chr_name = "rDNAm"
 ) {
-    rdna_gr <- GRanges(
+    rdna_gr <- GenomicRanges::GRanges(
         seqnames = rDNA_chr_name,
-        ranges = IRanges(start = 1, end = 1e9)
+        ranges = IRanges::IRanges(start = 1, end = 1e9)
     )
-    meth_rdna_raw <- selectByOverlap(meth_object, rdna_gr)
+    meth_rdna_raw <- methylKit::selectByOverlap(meth_object, rdna_gr)
     return(meth_rdna_raw)
 }
